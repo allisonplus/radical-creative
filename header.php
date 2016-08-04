aae53b637eff3cc55c661813c33f499ec960ac39
Allison Tarr <allison@webdevstudios.com>
2016-08-04 15:38:50 -0600

    add placeholder reminder for banner + adjust 404 markup

--- header.php
+++ header.php
@@ -32,6 +32,7 @@
 		<div class="wrap">
 
 			<div class="site-branding">
+				<?php // BANNER IMAGE PLACED HERE ACROSS ALL PAGES ?>
 				<?php if ( is_front_page() && is_home() ) : ?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php else : ?>
