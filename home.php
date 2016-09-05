d32f11d2f8aab1f79ff97c2fda0d40698396d28a
Allison Tarr <allison@webdevstudios.com>
2016-09-05 12:39:39 -0400

    create template for writing page

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the Writings (Blog) main page.
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Radical Creative Sanctuary 2.0
+ */
+
+get_header(); ?>
+
+	<div class="wrap">
+		<div class="primary content-area">
+			<main id="main" class="site-main" role="main">
+
+				<?php
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'template-parts/content', 'page' );
+
+				endwhile; // End of the loop.
+				?>
+
+			</main><!-- #main -->
+		</div><!-- .primary -->
+
+	</div><!-- .wrap -->
+
+<?php get_footer(); ?>
