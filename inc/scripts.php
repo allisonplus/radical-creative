5dc026293c53acd943dc0e8321a3c43f6b4ab46f
Allison Tarr <hello@allisontarr.com>
2017-08-23 09:49:31 -0400

    add analytics hook

--- inc/scripts.php
+++ inc/scripts.php
@@ -122,3 +122,23 @@ function rcs_include_svg_icons() {
 		require_once( $svg_icons );
 	}
 }
+
+
+
+add_action( 'wp_footer', 'rcs_google_analytics' );
+/**
+ * Add Google Analytics Tracking.
+ */
+function rcs_google_analytics() {
+	// Hook into the footer. ?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-105139927-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
+	<?php
+}
