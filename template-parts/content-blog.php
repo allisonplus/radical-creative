eeab095cc32b42eee21eaecb427098a6baf2c0b6
Allison Tarr <hello@allisontarr.com>
2020-07-15 13:29:32 -0400

    get rid of blog post meta for now, create fallback if no thumbnail image

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -11,12 +11,14 @@
 
 <article <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'blog' ); } ?>
+		<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'blog' ); }
+		else echo '<div class="thumbnail-filler"></div>';
+		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		<?php rcs_get_writing_meta_data(); ?>
+		<?php //rcs_get_writing_meta_data(); ?>
 
 		<?php
 			echo rcs_get_the_excerpt( $args = array( 'length' => 55 ) ); // WPCS: XSS OK.
